c2f19c881b9a4198d14be72205cecf703c702b26
Semyon Livinskiy <ghurcb5@gmail.com>
2025-11-03 19:05:25 +0600

    commit 2

--- Domain/Interfaces/IMovable.cs
+++ Domain/Interfaces/IMovable.cs
@@ -3,7 +3,7 @@ namespace SpaceBattle2025.Domain.Interfaces;
 public interface IMovable
 {
     (int X, int Y) Position { get; set; }
-    (int dX, int dY) Velocity { get; set; }
+    (int dX, int dY) Velocity { get;}
 
     void Move();
 }
---
+++ Domain/Interfaces/IRotatable.cs
@@ -0,0 +1,7 @@
+namespace Spaceship2025.Domain.Interfaces;
+
+public interface Rotatable
+{
+    double Angle { get; set; }
+    double AngularVelocity {get;}
+}
